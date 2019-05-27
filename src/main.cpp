4efec2d750f9ac9fc962d5498dd3b507977670ce
unknown <ag9217@ic.ac.uk>
2019-05-27 15:32:31 +0100

    added fstream library

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include "..\include\sNumber.h" //sudoku number class
 #include "..\include\board.h" //sudoku board class
 using namespace std;
 
 int main()
 {
-    cout << "Hello" << endl;
     bool solved = 0;
 
     Board myboard;
-
     myboard.draw();
 
     while (!solved)
